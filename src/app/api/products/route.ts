f9db32e224260e7bb9cece3c39190a46d0159350
vani0-0 <van973vv@gmail.com>
2025-06-03 07:52:36 +0800

    fixed eslint warning

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,6 +1,6 @@
 import { data } from "@/data/sample_data";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
+export async function GET() {
   return NextResponse.json({ data }, { status: 200 });
 }
